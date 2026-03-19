53c2ac991079c5589cdfefa733b83e5f4c667bf9
gv-26 <galerievarinchi@gmail.com>
2026-03-19 13:46:54 +0530

    Cast PrismaClient options as any for typescript unblocker

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -10,6 +10,6 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient({
       url: process.env.DATABASE_URL
     }
   }
-});
+} as any);
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
