b545f3fc39cede96d8bf4cc2215085f2f0220855
gv-26 <galerievarinchi@gmail.com>
2026-03-20 13:39:14 +0530

    Transpile AWS SDK for production bundling

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['jose', 'resend'],
+  transpilePackages: ['@aws-sdk/client-s3'],
 };
 
 export default nextConfig;
